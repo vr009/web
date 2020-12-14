b279f349fa36fbc1c8f1c47d900b0290fab9f040
vr009 <55545103+vr009@users.noreply.github.com>
2020-12-14 18:20:45 +0300

    Update init.sh

--- etc/init.sh
+++ etc/init.sh
@@ -1,5 +1,5 @@
 #linkin to the configuration in etc 
-sudo unlink /etc/nginx/sites-enabled/default
+#sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
 
