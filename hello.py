40ef57bc6bf641b34c914c0343ddf1f471957ecb
vr009 <55545103+vr009@users.noreply.github.com>
2020-12-21 22:07:53 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,15 +2,16 @@ from urllib.parse import urlparse, parse_qs
 
 def my_wsgi(environ,start_response)
   if environ.get("REQUEST_METHOD") == "GET"
-    answer_string = environ.get("QUERY_STRING")
-    temp_body = answer_string.split("&")
-    body = "\n".join(temp_body)
+    #answer_string = environ.get("QUERY_STRING")
+    #temp_body = answer_string.split("&")
+    #body = "\n".join(temp_body)
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     status = '200 OK'
     headers = [
     ('Content-Type','text/plain')
     ]
   start_response(status,headers)
-  return [body]
+  return body
    
 #body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
 #return body
